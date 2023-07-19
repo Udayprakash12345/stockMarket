7198d2afb2a3bb13563fb43f7ea59a7037784bb0
Udayprakash12345 <udayprakash2071996@gmail.com>
2023-07-19 15:59:51 +0530

    first commited

---
+++ myjs.js
@@ -0,0 +1,10 @@
+// ApiKey
+// Alpha Vantage Stock API
+const ApiKey='D5INP92YUQFHO3HF';
+const searchBox=document.getElementById('search-box');
+const searchSymbol=document.getElementById('search-symbol');
+const watchList=document.getElementById('watch-list');
+
+
+
+
